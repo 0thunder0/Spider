1f52de75cf304b6a7b897ae1bda597fb1f1cd191
“0thunder0” <myth@7liege.cc>
2018-11-09 08:59:34 +0000

    20181109

--- weibo/weibo/spiders/wb.py
+++ weibo/weibo/spiders/wb.py
@@ -7,9 +7,10 @@ from pyquery import PyQuery as pq
 class WbSpider(scrapy.Spider):
     name = 'wb'
     allowed_domains = ['weibo.com']
-    start_urls = ['https://m.weibo.cn/']
+    start_urls = ['https://m.weibo.cn/u/1312066630']
     
     def __init__(self):
+
         self.headers={
                 'Host':'m.weibo.cn',
                 'User-Agent':'Mozilla/5.0 (Linux; U; Android 4.4.4; Nexus 5 Build/KTU84P) AppleWebkit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',

9436e0e7ccbcf6cee85aa5939876c1d831e56052
0thunder0 <myth@7liege.cc>
2018-11-10 22:33:41 +0800

    1110

--- chrome.py
+++ chrome.py
@@ -1,25 +1,18 @@
 import time,requests
 from selenium import webdriver
 from pyvirtualdisplay import Display
-from bs4 import BeautifulSoup as bs
 from pyquery import PyQuery as pq
 
-display=Display(visible=0,size=(800,800))
-display.start()
-Dr=webdriver.Chrome()
-#Dr.get('http://www.cnblogs.com/')
-#pyquery----------------------------------------
+#display=Display(visible=0,size=(800,800))
+#display.start()
+Dr=webdriver.Firefox()
 Dr.get('http://www.laozuo.org/')
-Dr.get('')
 req=pq(Dr.page_source)
 a_list=req('.main li h2 a').items()
 for a_li in a_list:
     print(a_li.attr('href'))
     print(a_li.text())
 
-
-
-
 Dr.close()
 Dr.quit()
-display.stop()
+#display.stop()

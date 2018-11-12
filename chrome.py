fca898c8b026bcd3b4ffc9d1d51bfc1306c14b79
“0thunder0” <myth@7liege.cc>
2018-11-12 06:21:16 +0000

    增加selenium

--- chrome.py
+++ chrome.py
@@ -5,14 +5,26 @@ from pyquery import PyQuery as pq
 
 #display=Display(visible=0,size=(800,800))
 #display.start()
-Dr=webdriver.Firefox()
-Dr.get('http://www.laozuo.org/')
-req=pq(Dr.page_source)
-a_list=req('.main li h2 a').items()
-for a_li in a_list:
-    print(a_li.attr('href'))
-    print(a_li.text())
 
-Dr.close()
-Dr.quit()
+# 设置ChromeDriver不加载图片
+chrome_opt = webdriver.ChromeOptions()
+prefs = {"profile.managed_default_content_settings.images": 2}
+chrome_opt.add_experimental_option("prefs", prefs)
+
+browser=webdriver.Firefox()
+browser.get('https://www.weibo.cn')
+#登录微博
+time.sleep(15)
+browser.find_element_by_css_selector("#loginname").send_keys("your_username")
+browser.find_element_by_css_selector(".info_list.password input[node-type='password']").send_keys("your_password")
+browser.find_element_by_css_selector(".info_list.login_btn a[node-type='submitBtn']").click()
+#Selenium模拟鼠标下拉
+#这样的操作是通过JS脚本来进行的
+for i in range(3):
+        browser.execute_script("window.scrollTo(0, document.body.scrollHeight); var lenOfPage=document.body.scrollHeight; return lenOfPage;")
+        time.sleep(3)
+        
+
+browser.close()
+browser.quit()
 #display.stop()
--- weibo/weibo/chromeMiddleWare.py
+++ weibo/weibo/chromeMiddleWare.py
@@ -11,7 +11,8 @@ class middleware_chrome(object):
         #判断该spider是否为我们的目标
         if spiders.browser:
             spiders.browser.get(request.url)
-
+            #通过按向下键将页面滚动条拖到底部
+            driver.find_element_by_xpath("//*[@id='wrapper_wrapper']").send_keys(Keys.DOWN)
             #直接返回给spider，而非再传给downloader 
             return HtmlResponse(url=spider.browser.current_url, body=spider.browser.page_source, encoding="utf-8", request=request)
 

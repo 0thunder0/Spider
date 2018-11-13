d0f3ed4a109767e7b1b046250e598647ce06df73
0thunder0 <myth@7liege.cc>
2018-11-13 21:18:48 +0800

    1113

--- xicidaili/xici_demo.py
+++ xicidaili/xici_demo.py
@@ -5,25 +5,28 @@ import time,random
 
 def xici_demo():
     t=random.randint(15,30)
-    display=Display(visible=0,size=(1600,1000))
-    display.start()
+    #display=Display(visible=0,size=(1600,1000))
+    #display.start()
     
-    browser=webdriver.Chrome()
+    browser=webdriver.Firefox()
     browser.get('http://www.xicidaili.com/nn/')
     time.sleep(t)
 
     req=pq(browser.page_source)
     #print(req)
-    ip_tables=req('#ip_list tbody tr')
-    print(ip_tables)
-    for data in ip_tables:
-        print('data数据：',data)
-        print('完整IP地址：')
+    ip_tables=req('#ip_list tbody tr').items()
+    for tab in ip_tables:
+        ip=tab('td:eq(1)').text()
+        port=tab('td:eq(2)').text()
+        print(ip+port)
+        addr=tab('td:eq(3) a').text()
+        alive_time=tab('td:eq(8)').text()
+
     
     
     browser.close()
     browser.quit()
-    display.stop()
+    #display.stop()
 
 if __name__ =='__main__':
     xici_demo()

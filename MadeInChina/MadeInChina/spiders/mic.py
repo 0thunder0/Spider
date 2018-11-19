9718ae23740c41954e15a4c3bd3079d21057088e
“0thunder0” <myth@7liege.cc>
2018-11-19 08:12:14 +0000

    中国制造外贸站加盟公司联系方式采集

--- MadeInChina/MadeInChina/spiders/mic.py
+++ MadeInChina/MadeInChina/spiders/mic.py
@@ -28,27 +28,19 @@ class MicSpider(scrapy.Spider):
         items=response.meta['item']
         s_data=pq(response.body)
         #print(response.body)
-        items['com_contact_detail']='联系人信息：'
-        com_contacts=s_data('.contact-customer .contact-customer-info .info-detail div').items()
-        for com_contact in com_contacts:
-            items['com_contact_name']=com_contact('.info-name').text()
-            items['com_contact_detail']=items['com_contact_detail']+com_contact('.info-item').text()
-            print(items['com_contact_name'],items['com_contact_detail'])
-        for detail_data in s_data('.contact-block .contact-info .info-item').items():
-            com_key=detail_data('.info-label').text().replace(' ','')
-            com_value=detail_data('.info-fields').text().replace(' ','')
-            print('前缀：',com_key,'后缀：',com_value)
-            if com_key==' Address:':
-                items['address']=com_value
-            elif com_key==' Local Time:':
-                items['local_time']=com_value
-            elif com_key==' Telephone:':
-                items['telephone']=com_value
-            elif com_key==' Mobile Phone:':
-                items['mobile_phone']=com_value
-            elif com_key==' Fax:':
-                items['fax']=com_value
-            elif com_key==' Website:':
-                items['website']=com_value
-            else:
-                print('无效的数据')
+        with open('mic.txt','a+') as f: 
+            com_name=s_data('.J-title-comName .title-txt a h1').text().replace(' ','')+'+'
+            f.write(com_name)
+            com_contacts=s_data('.contact-customer .contact-customer-info .info-detail div').items()
+            for com_contact in com_contacts:
+                f.writelines(com_contact('.info-name').text()+'+')
+                f.writelines(com_contact('.info-item').text())
+            f.write('+')
+            for detail_data in s_data('.contact-block .contact-info .info-item').items():
+                com_key=detail_data('.info-label').text().replace(' ','')
+                com_value=detail_data('.info-fields').text().replace(' ','')
+                if com_key.strip() and com_value.strip():
+                    f.writelines(com_key)
+                    f.writelines(com_value)
+                    f.write('+')
+            f.write('\n')

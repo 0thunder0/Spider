6ef5643ae6ef2d0c412f222bc5b13e9dec7d0c26
0thunder0 <myth@7liege.cc>
2019-01-29 21:46:00 +0800

    更新微博selenium下载

--- weibo/demo.py
+++ weibo/demo.py
@@ -18,13 +18,17 @@ time.sleep(random.randint(10,20))
 driver.get('https://weibo.com/p/1005052109243041/photos')
 #用js控制下拉
 js='document.documentElement.scrollTop=10000'
-for i in range(1,10):
-    if i==9:
-        break
+for i in range(1,2):
     driver.execute_script(js)
     time.sleep(60)
 page_source=driver.page_source
 data=pq(page_source)
+
+title=driver.title
+wb_file=title.split('的')[0]
+fpath=os.getcwd()+'/'+wb_file
+if os.path.exists(fpath) is False:
+    os.makedirs(fpath)
 
 img_urls=data('.photo_module .photo_cont a img').items()
 n=0
@@ -32,13 +36,12 @@ for img_cache in img_urls:
     img_url=img_cache.attr('src').split('?')[0]
     img_url=img_url.replace('//wxt.sinaimg.cn/thumb300','https://wx2.sinaimg.cn/large')
     n=n+1
-    urllib.request.urlretrieve(img_url,os.getcwd()+'/'+img_url.split('/')[-1])
+    urllib.request.urlretrieve(img_url,fpath+'/'+img_url.split('/')[-1])
     print('正在采集第%s个微博图片' %n,img_url)
 
+print(wb_file)
 time.sleep(random.randint(10,30))
-driver.get_screenshot_as_file('weibo_screenshot.png')
+#driver.get_screenshot_as_file('weibo_screenshot.png')
 #打印微博的标题和源代码
-title=driver.title
-print(title)
 driver.close()
 #display.stop()
